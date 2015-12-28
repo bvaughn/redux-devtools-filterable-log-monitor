ad331190cef49cbdbc4e4ba8bd49dea98be76574
Brian Vaughn <brian.david.vaughn@gmail.com>
2015-12-28 18:35:30 -0500

    Updated website style

--- website/Application.js
+++ website/Application.js
@@ -23,57 +23,67 @@ class Application extends Component {
 
     return (
       <div className={styles.Application}>
-        <h1>
-          redux-devtools-filterable-log-monitor
-          &nbsp;
-          <small>
-            <a href='https://github.com/bvaughn/redux-devtools-filterable-log-monitor'>view in Github</a>
-          </small>
-        </h1>
+        <a href='https://github.com/bvaughn/redux-devtools-filterable-log-monitor'>
+          <img
+            width='250'
+            height='123'
+            src='https://cloud.githubusercontent.com/assets/29597/12027069/779d8f3c-ad8f-11e5-8bac-2f6e8eed7192.png'
+            alt='redux devtools filterable log monitor logo'/>
+        </a>
+
+        <ul>
+          <li>Use the log monitor on the side to filter the Redux store.</li>
+          <li>(You can filter using strings or regular expressions!)</li>
+          <li>Use the buttons below to trigger a store update (adding more random data).</li>
+        </ul>
 
         <p>
-          Use the log monitor to the side to filter the Redux store.
-          (You can filter using strings or regular expressions!)
-          Use the buttons below to trigger a store update (adding more random data).
-        </p>
-
-        <p>
-          <button
-            className={styles.Button}
+          <UpdateButton
+            label='Array'
+            labelClass='array'
             onClick={udpateArray}
-          >
-            Update Array
-          </button>
-
-          <button
-            className={styles.Button}
+          />
+          <UpdateButton
+            label='List'
+            labelClass='array'
             onClick={udpateList}
-          >
-            Update List
-          </button>
-
-          <button
-            className={styles.Button}
+          />
+          <UpdateButton
+            label='Map'
+            labelClass='object'
             onClick={udpateMap}
-          >
-            Update Map
-          </button>
-
-          <button
-            className={styles.Button}
+          />
+          <UpdateButton
+            label='Object'
+            labelClass='object'
             onClick={udpateObject}
-          >
-            Update Object
-          </button>
+          />
         </p>
 
         <p>
-          Apologies for the basic demo; I hope to add more soon!
+          Apologies for the basic demo.
+          I hope to add more soon!
           In the meanwhile check out the <a href='https://github.com/bvaughn/redux-devtools-filterable-log-monitor'>documentation</a> to learn more.
         </p>
       </div>
     )
   }
+}
+
+function UpdateButton ({
+  label,
+  labelClass,
+  onClick
+}) {
+  return (
+    <button
+      className={styles.Button}
+      onClick={onClick}
+    >
+      <span className={styles.update}>Update</span>
+      <span className={styles[labelClass]}>{label}</span>
+    </button>
+  )
 }
 
 const selectors = () => ({})

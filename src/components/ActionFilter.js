78aea7a597d1ea39a3f09f1acaf86132f7aa86b1
Brian Vaughn <brian.david.vaughn@gmail.com>
2016-02-12 23:02:32 -0800

    Resoles #24 Fixes debounce issue

--- src/components/ActionFilter.js
+++ src/components/ActionFilter.js
@@ -1,59 +1,71 @@
 /** @flow */
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
 import debounce from 'lodash.debounce'
 import { setActionFilterText } from '../actions'
 
 const DEBOUNCE_TIME = 250
 
-ActionFilter.propTypes = {
-  actionFilterText: PropTypes.string,
-  dispatch: PropTypes.func.isRequired,
-  theme: PropTypes.object.isRequired
-}
+export default class ActionFilter extends Component {
+  static propTypes = {
+    actionFilterText: PropTypes.string,
+    dispatch: PropTypes.func.isRequired,
+    theme: PropTypes.object.isRequired
+  }
 
-export default function ActionFilter ({
-  actionFilterText,
-  dispatch,
-  theme
-}) {
-  function onActionFilterTextChange (event) {
-    const actionFilterText = event.target.value
+  constructor (props) {
+    super(props)
+
+    this._debouncedOnActionFilterTextChange = debounce(
+      this._onActionFilterTextChange.bind(this),
+      DEBOUNCE_TIME
+    )
+  }
+
+  render () {
+    const {
+      actionFilterText,
+      theme
+    } = this.props
+
+    return (
+      <div
+        style={{
+          flex: '0 0 auto',
+          display: 'flex',
+          flexDirection: 'row',
+          backgroundColor: theme.base01,
+          padding: '.75em',
+          fontSize: '.65em',
+          textTransform: 'uppercase',
+          fontWeight: 'bold'
+        }}
+      >
+        <input
+          ref='input'
+          style={{
+            flex: '0 0 100%',
+            borderRadius: '.25em',
+            lineHeight: '3em',
+            padding: '0 .75em',
+            border: 'none',
+            backgroundColor: theme.base06,
+            color: theme.base00
+          }}
+          type='text'
+          placeholder='Filter actions by type..'
+          defaultValue={actionFilterText}
+          onChange={this._debouncedOnActionFilterTextChange}
+        />
+      </div>
+    )
+  }
+
+  _onActionFilterTextChange () {
+    const actionFilterText = this.refs.input.value
+    const { dispatch } = this.props
+
     dispatch(setActionFilterText({
       actionFilterText
     }))
   }
-
-  // Debounce for better usability
-  const debouncedOnActionFilterTextChange = debounce(onActionFilterTextChange, DEBOUNCE_TIME)
-
-  return (
-    <div
-      style={{
-        flex: '0 0 auto',
-        display: 'flex',
-        flexDirection: 'row',
-        backgroundColor: theme.base01,
-        padding: '.75em',
-        fontSize: '.65em',
-        textTransform: 'uppercase',
-        fontWeight: 'bold'
-      }}
-    >
-      <input
-        style={{
-          flex: '0 0 100%',
-          borderRadius: '.25em',
-          lineHeight: '3em',
-          padding: '0 .75em',
-          border: 'none',
-          backgroundColor: theme.base06,
-          color: theme.base00
-        }}
-        type='text'
-        placeholder='Filter actions by type..'
-        defaultValue={actionFilterText}
-        onChange={debouncedOnActionFilterTextChange}
-      />
-    </div>
-  )
 }
--- src/components/FilterHeader.js
+++ src/components/FilterHeader.js
@@ -1,5 +1,5 @@
 /** @flow */
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
 import {
   setFilterByKeys,
   setFilterByValues,
@@ -10,206 +10,243 @@ import debounce from 'lodash.debounce'
 import 'date-format-lite'
 import MediaQuery from 'react-responsive'
 
-FilterHeader.propTypes = {
-  action: PropTypes.object.isRequired,
-  actionId: PropTypes.any.isRequired,
-  dispatch: PropTypes.func.isRequired,
-  monitorStateAction: PropTypes.object.isRequired,
-  theme: PropTypes.object.isRequired
-}
-
 const DEBOUNCE_TIME = 250
 
-export default function FilterHeader ({
-  action,
-  actionId,
-  dispatch,
-  monitorStateAction,
-  theme
-}) {
-  const {
-    expanded,
-    filterByKeys,
-    filterByValues,
-    filterText,
-    time
-  } = monitorStateAction
+export default class FilterHeader extends Component {
+  static propTypes = {
+    action: PropTypes.object.isRequired,
+    actionId: PropTypes.any.isRequired,
+    dispatch: PropTypes.func.isRequired,
+    monitorStateAction: PropTypes.object.isRequired,
+    theme: PropTypes.object.isRequired
+  }
 
-  function onFilterByKeysChange (event) {
+  constructor (props) {
+    super(props)
+
+    this._onFilterByKeysChange = this._onFilterByKeysChange.bind(this)
+    this._onFilterByValuesChange = this._onFilterByValuesChange.bind(this)
+    this._onFilterTextChange = this._onFilterTextChange.bind(this)
+    this._toggleExpanded = this._toggleExpanded.bind(this)
+
+    this._debouncedOnFilterTextChange = debounce(
+      this._onFilterTextChange,
+      DEBOUNCE_TIME
+    )
+  }
+
+  render () {
+    const {
+      action,
+      monitorStateAction,
+      theme
+    } = this.props
+
+    const {
+      expanded,
+      filterByKeys,
+      filterByValues,
+      filterText,
+      time
+    } = monitorStateAction
+
+    return (
+      <div
+        style={{
+          flex: '0 0 auto',
+          alignItems: 'center'
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            padding: '.5em',
+            backgroundColor: theme.base02,
+            color: theme.base06
+          }}
+        >
+          <div
+            style={{
+              display: 'inline-block',
+              marginRight: '5px',
+              borderLeft: '5px solid transparent',
+              borderRight: '5px solid transparent',
+              borderTopWidth: '5px',
+              borderTopStyle: 'solid',
+              cursor: 'pointer',
+              transition: '150ms',
+              transform: expanded ? 'rotateZ(0deg)' : 'rotateZ(-90deg)',
+              borderTopColor: expanded ? theme.base06 : theme.base05
+            }}
+            onClick={this._toggleExpanded}
+          />
+
+          <div
+            title={action.type}
+            onClick={this._toggleExpanded}
+            style={{
+              flex: '1 1 auto',
+              whiteSpace: 'nowrap',
+              textOverflow: 'ellipsis',
+              overflowX: 'hidden',
+              cursor: 'pointer'
+            }}
+          >
+            {action.type}
+
+            <MediaQuery query='(min-width: 1024px)'>
+              <span
+                style={{
+                  color: theme.base05,
+                  fontSize: '.6em',
+                  marginLeft: '.6em'
+                }}
+              >
+                {`(${time.format('H:mm:ss A')})`}
+              </span>
+            </MediaQuery>
+          </div>
+
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              flex: '0 0 auto',
+              marginLeft: '.5em',
+              fontSize: '.8em'
+            }}
+          >
+            <MediaQuery query='(min-width: 1024px)'>
+              <div
+                style={{
+                  flex: '0 0 auto'
+                }}
+              >
+                Filter by
+              </div>
+            </MediaQuery>
+            <label
+              style={{
+                flex: '0 0 auto',
+                marginLeft: '.5em',
+                fontWeight: 'normal',
+                color: theme.base0D
+              }}
+            >
+              <input
+                type='checkbox'
+                checked={filterByKeys}
+                onChange={this._onFilterByKeysChange}
+              />
+              Keys
+            </label>
+            <label
+              style={{
+                flex: '0 0 auto',
+                marginLeft: '.5em',
+                fontWeight: 'normal',
+                color: theme.base0B
+              }}
+            >
+              <input
+                type='checkbox'
+                checked={filterByValues}
+                onChange={this._onFilterByValuesChange}
+              />
+              Values
+            </label>
+          </div>
+        </div>
+
+        {expanded &&
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+              alignItems: 'center',
+              backgroundColor: theme.base01,
+              color: theme.base06,
+              padding: '.75em',
+              fontSize: '.65em',
+              textTransform: 'uppercase',
+              fontWeight: 'bold'
+            }}
+          >
+            <input
+              ref='input'
+              style={{
+                flex: '0 0 100%',
+                borderRadius: '.25em',
+                lineHeight: '3em',
+                padding: '0 .75em',
+                border: 'none',
+                backgroundColor: theme.base06,
+                color: theme.base00
+              }}
+              type='text'
+              placeholder='Filter by /regex/i or "string"'
+              defaultValue={filterText}
+              onChange={this._debouncedOnFilterTextChange}
+            />
+          </div>
+        }
+      </div>
+    )
+  }
+
+  _onFilterByKeysChange (event) {
     const filterByKeys = event.target.checked
+    const {
+      actionId,
+      dispatch
+    } = this.props
+
     dispatch(setFilterByKeys({
       actionId,
       filterByKeys
     }))
   }
 
-  function onFilterByValuesChange (event) {
+  _onFilterByValuesChange (event) {
     const filterByValues = event.target.checked
+    const {
+      actionId,
+      dispatch
+    } = this.props
+
     dispatch(setFilterByValues({
       actionId,
       filterByValues
     }))
   }
 
-  function onFilterTextChange (event) {
-    const filterText = event.target.value
+  _onFilterTextChange (event) {
+    const filterText = this.refs.input.value
+    const {
+      actionId,
+      dispatch
+    } = this.props
+
     dispatch(setFilterText({
       actionId,
       filterText
     }))
   }
 
-  function toggleExpanded () {
+  _toggleExpanded () {
+    const {
+      actionId,
+      dispatch,
+      monitorStateAction
+    } = this.props
+
+    const { expanded } = monitorStateAction
+
     dispatch(setExpanded({
       actionId,
       expanded: !expanded
     }))
   }
-
-  // Debounce for better usability
-  const debouncedOnFilterTextChange = debounce(onFilterTextChange, DEBOUNCE_TIME)
-
-  return (
-    <div
-      style={{
-        flex: '0 0 auto',
-        alignItems: 'center'
-      }}
-    >
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          padding: '.5em',
-          backgroundColor: theme.base02,
-          color: theme.base06
-        }}
-      >
-        <div
-          style={{
-            display: 'inline-block',
-            marginRight: '5px',
-            borderLeft: '5px solid transparent',
-            borderRight: '5px solid transparent',
-            borderTopWidth: '5px',
-            borderTopStyle: 'solid',
-            cursor: 'pointer',
-            transition: '150ms',
-            transform: expanded ? 'rotateZ(0deg)' : 'rotateZ(-90deg)',
-            borderTopColor: expanded ? theme.base06 : theme.base05
-          }}
-          onClick={toggleExpanded}
-        />
-
-        <div
-          title={action.type}
-          onClick={toggleExpanded}
-          style={{
-            flex: '1 1 auto',
-            whiteSpace: 'nowrap',
-            textOverflow: 'ellipsis',
-            overflowX: 'hidden',
-            cursor: 'pointer'
-          }}
-        >
-          {action.type}
-
-          <MediaQuery query='(min-width: 1024px)'>
-            <span
-              style={{
-                color: theme.base05,
-                fontSize: '.6em',
-                marginLeft: '.6em'
-              }}
-            >
-              {`(${time.format('H:mm:ss A')})`}
-            </span>
-          </MediaQuery>
-        </div>
-
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            flex: '0 0 auto',
-            marginLeft: '.5em',
-            fontSize: '.8em'
-          }}
-        >
-          <MediaQuery query='(min-width: 1024px)'>
-            <div
-              style={{
-                flex: '0 0 auto'
-              }}
-            >
-              Filter by
-            </div>
-          </MediaQuery>
-          <label
-            style={{
-              flex: '0 0 auto',
-              marginLeft: '.5em',
-              fontWeight: 'normal',
-              color: theme.base0D
-            }}
-          >
-            <input
-              type='checkbox'
-              checked={filterByKeys}
-              onChange={onFilterByKeysChange}
-            />
-            Keys
-          </label>
-          <label
-            style={{
-              flex: '0 0 auto',
-              marginLeft: '.5em',
-              fontWeight: 'normal',
-              color: theme.base0B
-            }}
-          >
-            <input
-              type='checkbox'
-              checked={filterByValues}
-              onChange={onFilterByValuesChange}
-            />
-            Values
-          </label>
-        </div>
-      </div>
-
-      {expanded &&
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-            alignItems: 'center',
-            backgroundColor: theme.base01,
-            color: theme.base06,
-            padding: '.75em',
-            fontSize: '.65em',
-            textTransform: 'uppercase',
-            fontWeight: 'bold'
-          }}
-        >
-          <input
-            style={{
-              flex: '0 0 100%',
-              borderRadius: '.25em',
-              lineHeight: '3em',
-              padding: '0 .75em',
-              border: 'none',
-              backgroundColor: theme.base06,
-              color: theme.base00
-            }}
-            type='text'
-            placeholder='Filter by /regex/i or "string"'
-            defaultValue={filterText}
-            onChange={debouncedOnFilterTextChange}
-          />
-        </div>
-      }
-    </div>
-  )
 }
